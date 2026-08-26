74a2e6853b9237554f5084c51ed498ca2eb3cc48
MutebiI <mutebiibrahim503@gmail.com>
2026-08-26 13:16:59 +0300

    Initial commit: ON-DEMAND AGRO-FARM website

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+# 1. Add all files
+git add -A
+
+# 2. Commit
+git commit -m "Initial commit: ON-DEMAND AGRO-FARM website"
+
+# 3. Connect to your repository
+git remote add origin https://github.com/MutebiI/on-demand-agro-farm.git
+
+# 4. Push
+git branch -M main
+git push -u origin main
